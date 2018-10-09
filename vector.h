8d08033ac51f5451b43c00d13da72d8e31eb4249
Yava2023 <marsenlennart53@googlemail.com>
2018-10-09 14:54:55 +0200

    Header

---
+++ vector.h
@@ -0,0 +1,13 @@
+#ifndef VECTOR_H_
+#define VECTOR_H_
+
+struct Vector3f {
+  float x, y, z;
+};
+
+std::ostream& operator<<(std::ostream& os, const Vector3f& v);
+float operator*(const Vector3f& v1, const Vector3f& v2);
+float norm(const Vector3f& v);
+float angle(const Vector3f& v1, const Vector3f& v2);
+
+#endif  // VECTOR_H_
